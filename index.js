6650b283c94c84b8e98ab3480db41c5b2284f159
Charlie Tekulve <54796991+chatek87@users.noreply.github.com>
2024-08-04 10:09:13 -0400

    add difficulty selector

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Card {
 class Game {
     constructor() {
         this.difficulty = getDifficulty();
-        this.emojiList = generateEmojiList();
+        this.emojiList = generateEmojiList(this.difficulty);
     }
 }
 
@@ -19,10 +19,31 @@ function getEmoji() {
 // get emoji for specific card, remove it from the emoji list
 }
 
-function generateEmojiList() {
-// dynamically generate an array of emojis based on game params (difficulty)
+function generateEmojiList(difficulty) {
+    if (difficulty) {
+        // dynamically generate an array of emojis based on game params (difficulty)
+
+    } else {
+        console.error("no difficulty provided");
+        return null;
+    }
 }
 
 function getDifficulty() {
 // get difficulty based on user selection, use this as arg to generateEmojiList()
+    const difficultySelector = document.getElementById("difficulty-selector");
+    const selection = difficultySelector.querySelector('input[name="difficulty"]:checked');
+
+    if (selection) {
+        return selection.value;
+    } else {
+        console.error("no difficulty selected");
+        return null;
+    }
+
 }
+
+// U+1F600 - U+1FAAA
+
+// make event handler for 'shuffle-deal' button
+// it starts a new game based on difficulty level
